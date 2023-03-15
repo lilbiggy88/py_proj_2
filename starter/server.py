0aa91e9e0a199a5b9c6583e3490684feb5a364fd
John Biggerstaff <john.r.biggerstaff@gmail.com>
2023-03-14 19:32:54 -0600

    part 4 complete

---
+++ starter/server.py
@@ -0,0 +1,24 @@
+from flask import Flask, render_template
+
+app = Flask(__name__)
+
+
+@app.route('/')
+def home():
+    return render_template("index.html")
+
+@app.route("/cupcakes")
+def all_cupcakes():
+    return render_template("cupcakes.html")
+
+app.route("/cupcake_individual")
+def individual_cupcake():
+    return render_template("individual-cupcake.html") 
+
+app.route("/order")
+def order():
+    return render_template('order.html')
+
+if __name__ == '__main__':
+    app.env = "development"
+    app.run(debug=True, port = 8000, host = "localhost")
